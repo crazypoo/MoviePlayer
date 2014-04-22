7981ccb07e50782bfbe46018043598de607f988d
crazypoo <273277355@qq.com>
2014-04-22 10:22:32 +0800

    Initial Commit

---
+++ MoviePlayer/PooViewController.h
@@ -0,0 +1,13 @@
+//
+//  PooViewController.h
+//  MoviePlayer
+//
+//  Created by crazypoo on 14-4-22.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PooViewController : UIViewController
+
+@end
---
+++ MoviePlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MoviePlayer
+//
+//  Created by crazypoo on 14-4-22.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PooAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PooAppDelegate class]));
+    }
+}
